d87322e8d93a8bfd19814a4ecd9fa52483ab8c0d
chenyu <409316036@qq.com>
2018-09-13 21:01:36 +0800

    初始化项目

---
+++ my_springboot_learn/src/main/java/com/example/my_springboot_learn/MySpringbootLearnApplication.java
@@ -0,0 +1,12 @@
+package com.example.my_springboot_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootLearnApplication.class, args);
+	}
+}
